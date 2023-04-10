7cdf115ed5795c47781757fdd5660ae5074275c7
Moein <pistolparody@gmail.com>
2023-04-10 09:34:29 +0330

    Added pymunk example

--- core/common_resources.py
+++ core/common_resources.py
@@ -3,5 +3,5 @@ from core.common_names import *
 from core.event_holder import EventHolder
 
 screen:Optional[pg.Surface] = None
-event_holder:Optional[pg.Surface] = None
+event_holder:Optional[EventHolder] = None
 game = None
