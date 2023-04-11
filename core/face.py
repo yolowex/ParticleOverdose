3ea801192dc8a1b6831070fdde800a95cd0685ee
Moein <pistolparody@gmail.com>
2023-04-11 16:17:00 +0330

    Added face

---
+++ core/face.py
@@ -0,0 +1,9 @@
+from core.common_names import *
+from core.common_functions import *
+import core.common_resources as cr
+from core.sprite import Sprite
+
+class Face:
+
+    def __init__( self ):
+        self.eye = 0
