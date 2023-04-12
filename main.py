2bdc9b3bc094bbad3c977f95f2b7b8438b8d736d
Moein <pistolparody@gmail.com>
2023-04-12 14:15:35 +0330

    Added basic camera

--- main.py
+++ main.py
@@ -13,26 +13,30 @@ res = "./pic_res.png"
 
 pg.init()
 
-cr.screen = pg.display.set_mode([900,640],SCALED | FULLSCREEN)
+cr.screen = pg.display.set_mode([900, 640], SCALED | FULLSCREEN)
 cr.event_holder = EventHolder()
 cr.event_holder.should_render_debug = False
 cr.event_holder.determined_fps = 1000
 cr.game = Game()
 cr.game.init()
 
+font = pg.font.SysFont('monospace', 30)
+fps_text = lambda : font.render(f"FPS :{int(cr.event_holder.final_fps)}", True, "white")
 
-font = pg.font.SysFont('monospace',30)
-fps_text = lambda: font.render(f"FPS :{int(cr.event_holder.final_fps)}",True,"white")
-
-while not cr.event_holder.should_quit:
-    if K_F3 in cr.event_holder.pressed_keys:
+while not cr.event_holder.should_quit :
+    if K_F3 in cr.event_holder.pressed_keys :
         cr.event_holder.should_render_debug = not cr.event_holder.should_render_debug
 
     cr.event_holder.get_events()
     cr.game.check_events()
     cr.game.render()
 
-    if cr.event_holder.should_render_debug:
-        cr.screen.blit(fps_text(),(0,0))
+    if cr.event_holder.should_render_debug :
+        cr.screen.blit(fps_text(), (0, 0))
 
-    pg.display.flip()
+    p = cr.game.player.center
+    cr.screen.scroll(-int(p.x) + int(cr.screen.get_width() * .5),
+        -int(p.y) + int(cr.screen.get_height() * .8))
+    pg.display.update()
+
+    # pg.image.save(cr.screen, "./dump.jpg")
