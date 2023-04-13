4f1cca488073c3cc67c588b14e60095f783bec44
Moein <pistolparody@gmail.com>
2023-04-13 21:52:53 +0330

    Revert "Merge pull request #1 from mmdmoa/web"
    This reverts commit a535d577734ed7c3c7f0d839a57771b84b3d4cec, reversing
    changes made to b7640f2539aa23fb031660887816146d61160f86.

--- core/common_names.py
+++ core/common_names.py
@@ -5,5 +5,4 @@ import pygame as pg
 from pygame.locals import *
 from typing import Optional
 
-from pygame import Vector2,Surface,Color
-from pygame import Rect as FRect
+from pygame import Vector2,FRect,Surface,Color
