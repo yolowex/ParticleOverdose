9f687d4f7ef74781746eb45a84a06d20f638bc0b
Moein <pistolparody@gmail.com>
2023-04-15 21:27:59 +0330

    Added special attack shortcuts

--- core/sword.py
+++ core/sword.py
@@ -45,16 +45,17 @@ class Sword :
             delta = 0.01
 
         count = int(delta * 100)
-        if IS_WEB:
+        if IS_WEB :
             count *= 6
 
         return count
+
 
     # @property
     # def is_active( self ):
     #     return self.distance > 0.25
 
-    def reset_sword( self ):
+    def reset_sword( self ) :
         self.is_attacking = False
         self.is_retrieving = False
         self.angle = 0
@@ -62,6 +63,7 @@ class Sword :
         self.move_timer = None
         self.timer = None
         self.rotate_sword()
+
 
     def rotate_sword( self ) :
         swords = [i.transformed_surface for i in [self.sword_right, self.sword_left]]
@@ -118,7 +120,7 @@ class Sword :
         b = pbr.lerp(pbl, 0.5)
         t = ptr.lerp(ptl, 0.5)
         lerp_value = 0.3
-        if self.name == 'death':
+        if self.name == 'death' :
             lerp_value = 0.7
         c = b.lerp(t, lerp_value)
 
@@ -149,19 +151,49 @@ class Sword :
             self.is_active = True
 
 
-
     def check_events( self ) :
         self.check_attack()
         self.angle += self.angle_power
 
         if K_f in cr.event_holder.pressed_keys :
             self.attack(ATTACK_NORMAL)
-        if K_v in cr.event_holder.pressed_keys:
-            if not self.is_attacking and not self.is_retrieving:
+        if K_v in cr.event_holder.pressed_keys :
+            if not self.is_attacking and not self.is_retrieving :
                 self.attack(ATTACK_SPECIAL)
-            elif not self.was_thrown and not self.is_attacking and not self.is_retrieving:
+            elif not self.was_thrown :
                 self.reset_sword()
                 self.timer = now()
+
+        if not self.was_thrown :
+            if K_q in cr.event_holder.pressed_keys and 'evil' not in cr.game.player.locked_swords_list :
+                self.reset_sword()
+                self.update_sword("evil")
+                self.attack(ATTACK_SPECIAL)
+
+            if K_w in cr.event_holder.pressed_keys and 'desire' not in cr.game.player.locked_swords_list :
+                self.reset_sword()
+                self.update_sword("desire")
+                self.attack(ATTACK_SPECIAL)
+
+            if K_e in cr.event_holder.pressed_keys and 'light' not in cr.game.player.locked_swords_list :
+                self.reset_sword()
+                self.update_sword("light")
+                self.attack(ATTACK_SPECIAL)
+
+            if K_r in cr.event_holder.pressed_keys and 'hawk' not in cr.game.player.locked_swords_list :
+                self.reset_sword()
+                self.update_sword("hawk")
+                self.attack(ATTACK_SPECIAL)
+
+            if K_t in cr.event_holder.pressed_keys and 'blood' not in cr.game.player.locked_swords_list :
+                self.reset_sword()
+                self.update_sword("blood")
+                self.attack(ATTACK_SPECIAL)
+
+            if K_g in cr.event_holder.pressed_keys and 'death' not in cr.game.player.locked_swords_list :
+                self.reset_sword()
+                self.update_sword("death")
+                self.attack(ATTACK_SPECIAL)
 
         self.check_activeness()
 
@@ -173,10 +205,9 @@ class Sword :
 
     def swing_attack( self, swing_speed: float = 1, release_speed: float = 0.5,
             swing_amount: float = 135 ) :
-
         angle_m = 1
         points = self.rotated_points_right
-        if cr.game.player.facing == LEFT:
+        if cr.game.player.facing == LEFT :
             points = self.rotated_points_left
             angle_m = -1
 
@@ -184,15 +215,15 @@ class Sword :
         if IS_WEB :
             web_m = 3
 
-        for _ in range(random.randint(1,self.particle_delta_count * 5)):
-            b = points[2].lerp(points[3],0.5)
-            t = points[0].lerp(points[1],0.5)
+        for _ in range(random.randint(1, self.particle_delta_count * 5)) :
+            b = points[2].lerp(points[3], 0.5)
+            t = points[0].lerp(points[1], 0.5)
 
             shoot_point = b.lerp(t, random.uniform(0.2, 0.8))
 
-            shoot_angle = (self.angle * angle_m) + random.randint(-10*web_m,10*web_m)
-            size = random.uniform(1,4.5)
-            self.add_particle(shoot_point,shoot_angle,size,)
+            shoot_angle = (self.angle * angle_m) + random.randint(-10 * web_m, 10 * web_m)
+            size = random.uniform(1, 4.5)
+            self.add_particle(shoot_point, shoot_angle, size, )
 
         self.last_attack_type = SWING
         m = swing_speed
@@ -214,7 +245,8 @@ class Sword :
 
         self.rotate_sword()
 
-    def throw_attack( self ):
+
+    def throw_attack( self ) :
         self.last_attack_type = THROW
         throw_angle = 90
         m = 0.5
@@ -231,28 +263,28 @@ class Sword :
         reverse = 1
 
         # retrieve
-        if (self.was_thrown and self.distance != self.original_distance) or self.cancel_throw:
+        if (self.was_thrown and self.distance != self.original_distance) or self.cancel_throw :
             self.is_attacking = False
             self.is_retrieving = True
             do_particles = True
             reverse *= -1
             self.distance -= cr.event_holder.delta_time * throw_speed
 
-            if self.cancel_throw or self.distance < self.original_distance:
+            if self.cancel_throw or self.distance < self.original_distance :
                 self.distance = self.original_distance
                 self.cancel_throw = False
 
         # rotate in
-        elif self.was_thrown and self.distance == self.original_distance:
+        elif self.was_thrown and self.distance == self.original_distance :
             self.angle -= self.angle_speed * m
-            if self.angle < 0:
+            if self.angle < 0 :
                 self.angle = 0
                 self.is_retrieving = False
                 self.is_attacking = False
                 self.was_thrown = False
                 self.timer = now()
 
-        else:
+        else :
             # rotate out
             if self.angle < throw_angle :
                 self.is_attacking = True
@@ -273,27 +305,21 @@ class Sword :
                     self.was_thrown = True
                     cr.game.player.teleport(Vector2(rect.center))
 
-
-
-
-
-
-        if do_particles:
+        if do_particles :
             angle_m = 1
             points = self.rotated_points_right
             if cr.game.player.facing == LEFT :
                 points = self.rotated_points_left
                 angle_m = -1
 
-
-            for _ in range(random.randint(1, self.particle_delta_count*5)) :
+            for _ in range(random.randint(1, self.particle_delta_count * 5)) :
                 bias = random.uniform(0, 1)
                 b = points[2].lerp(points[3], bias)
                 t = points[0].lerp(points[1], bias)
 
                 shoot_point = b.lerp(t, random.uniform(0.2, 0.8))
 
-                shoot_angle = (self.angle * angle_m) + random.randint(-7,7)
+                shoot_angle = (self.angle * angle_m) + random.randint(-7, 7)
                 shoot_angle = shoot_angle * -1 * reverse
 
                 size = random.uniform(1, 4.5)
@@ -302,7 +328,7 @@ class Sword :
         self.rotate_sword()
 
 
-    def swirling_throw_attack( self ):
+    def swirling_throw_attack( self ) :
         angle_m = 1
         points = self.rotated_points_right
         if cr.game.player.facing == LEFT :
@@ -313,16 +339,15 @@ class Sword :
         if IS_WEB :
             web_m = 3
 
-        for _ in range(random.randint(1, self.particle_delta_count*5)) :
+        for _ in range(random.randint(1, self.particle_delta_count * 5)) :
             b = points[2].lerp(points[3], 0.5)
             t = points[0].lerp(points[1], 0.5)
 
             shoot_point = b.lerp(t, random.uniform(0.2, 0.8))
 
-            shoot_angle = (self.angle * angle_m) + random.randint(-10*web_m,10*web_m)
+            shoot_angle = (self.angle * angle_m) + random.randint(-10 * web_m, 10 * web_m)
             size = random.uniform(1, 4.5)
             self.add_particle(shoot_point, shoot_angle, size, )
-
 
         self.last_attack_type = SWIRLING_THROW
         throw_angle = 90
@@ -333,51 +358,51 @@ class Sword :
         retrieve_speed = 15
         max_distance = 8
 
+
         # It's messy but I'm in a hurry
-        def current_distance_scale():
+        def current_distance_scale() :
             x = 1 - (self.distance / max_distance)
             x *= 2
-            if x >= 1:
+            if x >= 1 :
                 x = 1
-            if x <= 0.2:
+            if x <= 0.2 :
                 x = 0.2
             return x
 
+
         # retrieve
-        if self.was_thrown and self.distance != self.original_distance:
+        if self.was_thrown and self.distance != self.original_distance :
             self.angle += self.angle_speed * retrieve_m
             self.is_attacking = False
             self.is_retrieving = True
             self.distance -= cr.event_holder.delta_time * retrieve_speed * current_distance_scale()
-            if self.distance < self.original_distance:
+            if self.distance < self.original_distance :
                 self.distance = self.original_distance
                 self.angle = abs(self.angle)
                 self.angle = self.angle % 360
 
         # rotate out
-        elif self.was_thrown and self.distance == self.original_distance:
-
+        elif self.was_thrown and self.distance == self.original_distance :
             dirc = -1
-            if self.angle > 360-self.angle:
+            if self.angle > 360 - self.angle :
                 dirc = 1
 
             self.angle += self.angle_speed * rotate_in_out * dirc
-            if self.angle < 0 or self.angle > 360:
+            if self.angle < 0 or self.angle > 360 :
                 self.angle = 0
                 self.is_retrieving = False
                 self.is_attacking = False
                 self.was_thrown = False
                 self.timer = now()
-        else:
+        else :
             # rotate in
-            if self.angle < throw_angle:
+            if self.angle < throw_angle :
                 self.is_attacking = True
                 self.angle += self.angle_speed * rotate_in_out
                 if self.angle >= throw_angle :
                     self.angle = throw_angle
             # throw
-            elif self.angle >= throw_angle:
-
+            elif self.angle >= throw_angle :
                 self.angle += self.angle_speed * throw_m
                 self.distance += cr.event_holder.delta_time * throw_speed * current_distance_scale()
                 if self.distance > max_distance :
@@ -386,43 +411,41 @@ class Sword :
 
         self.rotate_sword()
 
-    def advance_attack( self,duration:float=0.5 ):
 
-
+    def advance_attack( self, duration: float = 0.5 ) :
         angle_m = 1
         points = self.rotated_points_right
         if cr.game.player.facing == LEFT :
             points = self.rotated_points_left
             angle_m = -1
 
-
-        for _ in range(random.randint(0,self.particle_delta_count*5)) :
+        for _ in range(random.randint(0, self.particle_delta_count * 5)) :
             bias = random.uniform(0, 1)
             b = points[2].lerp(points[3], bias)
             t = points[0].lerp(points[1], bias)
 
             shoot_point = b.lerp(t, random.uniform(0.2, 0.8))
 
-            shoot_angle = (self.angle * angle_m) + random.randint(-5,5)
+            shoot_angle = (self.angle * angle_m) + random.randint(-5, 5)
             shoot_angle = shoot_angle * -1
 
             size = random.uniform(1, 4.5)
             self.add_particle(shoot_point, shoot_angle, size, )
 
         dirc = cr.game.player.move_speed * 2.8
-        if cr.game.player.facing == LEFT:
+        if cr.game.player.facing == LEFT :
             dirc *= -1
 
         self.angle = 90
         self.rotate_sword()
         halt = False
-        if not cr.game.player.move(Vector2(dirc,0),True):
+        if not cr.game.player.move(Vector2(dirc, 0), True) :
             halt = True
 
-        if self.move_timer is None:
+        if self.move_timer is None :
             self.move_timer = now()
 
-        elif self.move_timer + duration < now() or halt:
+        elif self.move_timer + duration < now() or halt :
             self.angle = 0
             self.rotate_sword()
             self.move_timer = None
@@ -431,11 +454,10 @@ class Sword :
             return
 
 
-    def fly_attack( self,duration:float=0.5 ):
-
+    def fly_attack( self, duration: float = 0.5 ) :
         points = self.rotated_points_right
         dirc = cr.game.player.move_speed * 3
-        if cr.game.player.facing == LEFT:
+        if cr.game.player.facing == LEFT :
             dirc *= -1
             points = self.rotated_points_left
 
@@ -448,25 +470,25 @@ class Sword :
             points = self.rotated_points_left
             angle_m = -1
 
-        for _ in range(random.randint(0, self.particle_delta_count*5)) :
+        for _ in range(random.randint(0, self.particle_delta_count * 5)) :
             bias = random.uniform(0, 1)
             b = points[2].lerp(points[3], bias)
             t = points[0].lerp(points[1], bias)
 
-            shoot_point = b.lerp(t, random.uniform(0.2,0.8))
+            shoot_point = b.lerp(t, random.uniform(0.2, 0.8))
             shoot_angle = (self.angle * angle_m) + random.randint(-5, 5)
             shoot_angle += 180
             size = random.uniform(1, 4.5)
             self.add_particle(shoot_point, shoot_angle, size, )
 
         halt = False
-        if not cr.game.player.move(Vector2(0,-(abs(dirc))),True):
+        if not cr.game.player.move(Vector2(0, -(abs(dirc))), True) :
             halt = True
 
-        if self.move_timer is None:
+        if self.move_timer is None :
             self.move_timer = now()
 
-        elif self.move_timer + duration < now() or halt:
+        elif self.move_timer + duration < now() or halt :
             self.angle = 0
             self.rotate_sword()
             self.move_timer = None
@@ -474,34 +496,34 @@ class Sword :
             self.timer = now()
             return
 
+
     def check_attack( self ) :
         if not self.is_attacking and not self.is_retrieving :
             return
 
-
-        if self.attack_key == ATTACK_NORMAL:
+        if self.attack_key == ATTACK_NORMAL :
             self.swing_attack(swing_amount=90)
-        else:
-            if self.name == 'death':
+        else :
+            if self.name == 'death' :
                 self.throw_attack()
 
-            if self.name == 'blood':
-                self.swing_attack(1,0.1,360*3.1)
+            if self.name == 'blood' :
+                self.swing_attack(1, 0.1, 360 * 3.1)
 
-            if self.name == 'desire':
+            if self.name == 'desire' :
                 self.swirling_throw_attack()
 
-            if self.name == 'evil':
-                self.swing_attack(2,4,360*10.8)
+            if self.name == 'evil' :
+                self.swing_attack(2, 4, 360 * 10.8)
 
-            if self.name == 'light':
+            if self.name == 'light' :
                 self.advance_attack(0.5)
 
-            if self.name == 'hawk':
+            if self.name == 'hawk' :
                 self.fly_attack(0.5)
 
 
-    def add_particle( self, source: Vector2, angle, size,color:Optional[Color]=None ) :
+    def add_particle( self, source: Vector2, angle, size, color: Optional[Color] = None ) :
         if len(cr.game.particles) > cr.game.maximum_particles :
             return
 
@@ -511,29 +533,29 @@ class Sword :
         color = color
         if self.name == 'death' :
             color = BLACK
-            if spec:
-                power_scale = random.uniform(0,1)
+            if spec :
+                power_scale = random.uniform(0, 1)
         elif self.name == 'blood' :
             power_scale = 1
             color = Color(random.randint(200, 255), random.randint(0, 75), random.randint(0, 75))
         elif self.name == 'hawk' :
             power_scale = 5
-            color = Color(random.randint(0, 75), random.randint(0, 75),random.randint(200, 255))
+            color = Color(random.randint(0, 75), random.randint(0, 75), random.randint(200, 255))
         elif self.name == 'evil' :
             power_scale = 10
-            color = Color(random.randint(0, 75),random.randint(200, 255), random.randint(0, 75))
+            color = Color(random.randint(0, 75), random.randint(200, 255), random.randint(0, 75))
         elif self.name == 'desire' :
-            color = Color(random.randint(200, 255),random.randint(50, 80), random.randint(0, 50))
+            color = Color(random.randint(200, 255), random.randint(50, 80), random.randint(0, 50))
         elif self.name == 'light' :
             power_scale = 5
-            color = Color(random.randint(200,255),0,random.randint(0,255))
+            color = Color(random.randint(200, 255), 0, random.randint(0, 255))
             color.g = color.r
 
         age = random.uniform(0, 1)
-        particle = Particle(source, size, angle, age,color,True,True)
+        particle = Particle(source, size, angle, age, color, True, True)
         # particle.power = random.uniform(4,5)
         particle.power = 5
-        if IS_WEB:
+        if IS_WEB :
             particle.power = 3.8
         particle.power_decrease_scale = power_scale
         cr.game.particles.append(particle)
@@ -549,26 +571,28 @@ class Sword :
                 self.is_active = False
 
 
-    def attack( self,attack_key) :
-        if self.is_attacking or self.is_retrieving:
+    def attack( self, attack_key ) :
+        if self.is_attacking or self.is_retrieving :
             return
 
         self.attack_key = attack_key
         self.is_attacking = True
         self.is_active = True
 
-    def is_colliding( self ) -> bool:
+
+    def is_colliding( self ) -> bool :
         points = self.rotated_points_right
-        if cr.game.player.facing == LEFT:
+        if cr.game.player.facing == LEFT :
             points = self.rotated_points_left
 
         rect = polygon_to_rect(points)
 
-        for box in cr.game.inner_box_list:
-            if rect.colliderect(box):
+        for box in cr.game.inner_box_list :
+            if rect.colliderect(box) :
                 return True
 
         return False
+
 
     def render_debug( self, points ) :
         pg.draw.polygon(cr.screen, 'red', points, width=2)
@@ -585,7 +609,7 @@ class Sword :
             sword = self.rotated_sword_left
 
         points = [i.copy() for i in points]
-        move_points(points,cr.camera.x,cr.camera.y)
+        move_points(points, cr.camera.x, cr.camera.y)
         the_rect = polygon_to_rect(points)
 
         cr.screen.blit(sword, the_rect)
